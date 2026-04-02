f80b079b1afc198aa1fb457763f5b95d6e58c9b4
ghambrec <gabriel.hambrecht@gmail.com>
2026-04-02 13:47:06 +0200

    fixed mistake in ftp init.sh

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -6,7 +6,7 @@ FTP_USER_PASSWORD=$(cat /run/secrets/ftp_user_password)
 if ! id "$FTP_USER" >/dev/null 2>&1; then
     useradd -m -d /home/wordpress -G www-data "${FTP_USER}"
 else
-	usermod -aG www-data "{$FTP_USER}"
+	usermod -aG www-data "${FTP_USER}"
 fi
 
 echo "${FTP_USER}:${FTP_USER_PASSWORD}" | chpasswd
